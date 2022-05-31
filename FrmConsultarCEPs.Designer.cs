af9867b9b0ab1320d13935476cf7e9357625803b
AxelGeorg <axelgeorg16@gmail.com>
2022-05-31 17:59:40 -0300

    project windows forms app .Net framework

---
+++ FrmConsultarCEPs.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace ConsultarCEPS
+{
+    partial class FrmConsultarCEPs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCEP = new System.Windows.Forms.Label();
+            this.textBoxCep = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxEstado = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.textBoxCidade = new System.Windows.Forms.TextBox();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.textBoxBairro = new System.Windows.Forms.TextBox();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.textBoxRua = new System.Windows.Forms.TextBox();
+            this.lblRua = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Location = new System.Drawing.Point(13, 31);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(53, 25);
+            this.lblCEP.TabIndex = 0;
+            this.lblCEP.Text = "CEP";
+            // 
+            // textBoxCep
+            // 
+            this.textBoxCep.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxCep.Location = new System.Drawing.Point(18, 61);
+            this.textBoxCep.Name = "textBoxCep";
+            this.textBoxCep.Size = new System.Drawing.Size(420, 29);
+            this.textBoxCep.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxRua);
+            this.groupBox1.Controls.Add(this.lblRua);
+            this.groupBox1.Controls.Add(this.textBoxBairro);
+            this.groupBox1.Controls.Add(this.lblBairro);
+            this.groupBox1.Controls.Add(this.textBoxCidade);
+            this.groupBox1.Controls.Add(this.lblCidade);
+            this.groupBox1.Controls.Add(this.textBoxEstado);
+            this.groupBox1.Controls.Add(this.lblEstado);
+            this.groupBox1.Location = new System.Drawing.Point(12, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(818, 401);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resultado";
+            // 
+            // textBoxEstado
+            // 
+            this.textBoxEstado.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxEstado.Location = new System.Drawing.Point(6, 85);
+            this.textBoxEstado.Name = "textBoxEstado";
+            this.textBoxEstado.Size = new System.Drawing.Size(798, 29);
+            this.textBoxEstado.TabIndex = 3;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(1, 44);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(73, 25);
+            this.lblEstado.TabIndex = 2;
+            this.lblEstado.Text = "Estado";
+            // 
+            // textBoxCidade
+            // 
+            this.textBoxCidade.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxCidade.Location = new System.Drawing.Point(6, 174);
+            this.textBoxCidade.Name = "textBoxCidade";
+            this.textBoxCidade.Size = new System.Drawing.Size(798, 29);
+            this.textBoxCidade.TabIndex = 5;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Location = new System.Drawing.Point(1, 134);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(75, 25);
+            this.lblCidade.TabIndex = 4;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // textBoxBairro
+            // 
+            this.textBoxBairro.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxBairro.Location = new System.Drawing.Point(6, 262);
+            this.textBoxBairro.Name = "textBoxBairro";
+            this.textBoxBairro.Size = new System.Drawing.Size(798, 29);
+            this.textBoxBairro.TabIndex = 7;
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(1, 222);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(63, 25);
+            this.lblBairro.TabIndex = 6;
+            this.lblBairro.Text = "Bairro";
+            // 
+            // textBoxRua
+            // 
+            this.textBoxRua.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxRua.Location = new System.Drawing.Point(6, 348);
+            this.textBoxRua.Name = "textBoxRua";
+            this.textBoxRua.Size = new System.Drawing.Size(798, 29);
+            this.textBoxRua.TabIndex = 9;
+            // 
+            // lblRua
+            // 
+            this.lblRua.AutoSize = true;
+            this.lblRua.Location = new System.Drawing.Point(1, 308);
+            this.lblRua.Name = "lblRua";
+            this.lblRua.Size = new System.Drawing.Size(47, 25);
+            this.lblRua.TabIndex = 8;
+            this.lblRua.Text = "Rua";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLimpar.Image = global::ConsultarCEPS.Properties.Resources.limpar;
+            this.btnLimpar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpar.Location = new System.Drawing.Point(590, 525);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(117, 45);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Image = global::ConsultarCEPS.Properties.Resources.sair;
+            this.btnSair.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSair.Location = new System.Drawing.Point(713, 525);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(117, 45);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackgroundImage = global::ConsultarCEPS.Properties.Resources.busca_cep;
+            this.btnConsultar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnConsultar.Location = new System.Drawing.Point(550, 29);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(266, 83);
+            this.btnConsultar.TabIndex = 5;
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // FrmConsultarCEPs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 582);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxCep);
+            this.Controls.Add(this.lblCEP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmConsultarCEPs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consultar CEPs";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCEP;
+        private System.Windows.Forms.TextBox textBoxCep;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxRua;
+        private System.Windows.Forms.Label lblRua;
+        private System.Windows.Forms.TextBox textBoxBairro;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.TextBox textBoxCidade;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.TextBox textBoxEstado;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
+
---
+++ FrmConsultarCEPs.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultarCEPS
+{
+    public partial class FrmConsultarCEPs : Form
+    {
+        public FrmConsultarCEPs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.textBoxCep.Text))
+            {
+                MessageBox.Show("Informe um CEP válido!!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                LimparCamposFrm();
+
+                return;
+            }
+
+            using (var ws = new WSCorreios.AtendeClienteClient())
+            {
+                try
+                {
+                    var enderecoERP = ws.consultaCEP(this.textBoxCep.Text.Trim());
+
+                    this.textBoxEstado.Text = enderecoERP.uf    ;
+                    this.textBoxCidade.Text = enderecoERP.cidade;
+                    this.textBoxBairro.Text = enderecoERP.bairro;
+                    this.textBoxRua   .Text = enderecoERP.end   ;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCamposFrm();
+        }
+
+        private void LimparCamposFrm()
+        {
+            this.textBoxCep.Clear();
+            this.textBoxEstado.Clear();
+            this.textBoxCidade.Clear();
+            this.textBoxBairro.Clear();
+            this.textBoxRua.Clear();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
